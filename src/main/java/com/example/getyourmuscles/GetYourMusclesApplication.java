ef811e38dbd3e3fc40e52bbe50d2a3771d32300e
Marcin Dołowy <marcindolowy00@gmail.com>
2023-05-20 00:49:41 +0200

    Initial commit

---
+++ src/main/java/com/example/getyourmuscles/GetYourMusclesApplication.java
@@ -0,0 +1,13 @@
+package com.example.getyourmuscles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetYourMusclesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetYourMusclesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/getyourmuscles/GetYourMusclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getyourmuscles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetYourMusclesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
