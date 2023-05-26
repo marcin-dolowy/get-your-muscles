a8740d72afba3f8ff7221ec4911a865544093c37
Marcin Dołowy <marcindolowy00@gmail.com>
2023-05-27 01:20:20 +0200

    Add spring security for roles and add authentication after account creating

---
+++ src/main/java/com/example/getyourmuscles/security/auth/AuthenticationService.java
@@ -0,0 +1,47 @@
+package com.example.getyourmuscles.security.auth;
+
+import com.example.getyourmuscles.security.config.JwtService;
+import com.example.getyourmuscles.security.user.Role;
+import com.example.getyourmuscles.security.user.User;
+import com.example.getyourmuscles.security.user.UserRepository;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+
+        User user = userRepository
+                .findByEmail(request.getEmail())
+                .orElseThrow(() -> new NoSuchElementException("User not found"));
+
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+}
