8bf60dce0cbad4365229e0ebe749a2eb6059c20a
hubertf13 <*ZT8JWT_@MAq>
2023-05-28 19:01:50 +0200

    Add action on login button

--- src/main/ui/calendar/src/HomePage.js
+++ src/main/ui/calendar/src/HomePage.js
@@ -1,7 +1,25 @@
 import * as React from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 const HomePage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const navigate = useNavigate();
+
+    const logIn = async () => {
+        try {
+
+
+
+            navigate('/calendar');
+        } catch (e) {
+            setError(e.message);
+        }
+    }
+
     return (
         <section className="vh-100" style={{backgroundColor: "#508bfc"}}>
             <div className="container py-5 h-100">
@@ -9,30 +27,27 @@ const HomePage = () => {
                     <div className="col-12 col-md-8 col-lg-6 col-xl-5">
                         <div className="card shadow-2-strong" style={{borderRadius: "1rem"}}>
                             <div className="card-body p-5 text-center">
-
                                 <h3 className="mb-5">Sign in</h3>
-
                                 <div className="form-outline mb-4">
-                                    <input type="text" id="emailInput" className="form-control form-control-lg"/>
+                                    <input type="text" id="emailInput" className="form-control form-control-lg" value={email} onChange={e => setEmail(e.target.value)}/>
                                     <label className="form-label" htmlFor="emailInput">Email</label>
                                 </div>
 
                                 <div className="form-outline mb-4">
-                                    <input type="password" id="passwordInput"
-                                           className="form-control form-control-lg"/>
+                                    <input type="password" id="passwordInput" className="form-control form-control-lg" value={password} onChange={e => setPassword(e.target.value)}/>
                                     <label className="form-label" htmlFor="passwordInput">Password</label>
                                 </div>
 
-                                <div className="form-check d-flex justify-content-start mb-4">
-                                    <input className="form-check-input me-1" type="checkbox" value="" id="rememberMeInput"/>
-                                    <label className="form-check-label" htmlFor="rememberMeInput"> Remember
-                                        password </label>
-                                </div>
+                                {/*<div className="form-check d-flex justify-content-start mb-4">*/}
+                                {/*    <input className="form-check-input me-1" type="checkbox" value="" id="rememberMeInput"/>*/}
+                                {/*    <label className="form-check-label" htmlFor="rememberMeInput"> Remember*/}
+                                {/*        password </label>*/}
+                                {/*</div>*/}
 
                                 <div className="d-grid gap-2">
-                                    <Link className="btn btn-primary" to="/calendar">Login</Link>
+                                    <button className="btn btn-primary" onClick={logIn}>Login</button>
                                     <hr/>
-                                    <Link className="btn btn-danger" to="#">Sign in with google</Link>
+                                    <button className="btn btn-danger" to="#">Sign in with google</button>
                                 </div>
 
                             </div>
