622b0ad39bd8a45fb61111bdcad47a9e5bc11890
Marcin Dołowy <marcindolowy00@gmail.com>
2023-05-28 14:26:19 +0200

    Code refactor

--- src/main/java/com/example/getyourmuscles/paypal/PaypalConfig.java
+++ src/main/java/com/example/getyourmuscles/paypal/PaypalConfig.java
@@ -1,39 +1,35 @@
 package com.example.getyourmuscles.paypal;
 
+import com.paypal.base.rest.APIContext;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.paypal.api.payments.PaymentHistory;
-import com.paypal.base.rest.APIContext;
-import com.paypal.base.rest.OAuthTokenCredential;
-import com.paypal.base.rest.PayPalRESTException;
-
 @Configuration
 public class PaypalConfig {
 
-	@Value("${paypal.client.id}")
-	private String clientId;
-	@Value("${paypal.client.secret}")
-	private String clientSecret;
-	@Value("${paypal.mode}")
-	private String mode;
+    @Value("${paypal.client.id}")
+    private String clientId;
 
-	@Bean
-	public Map<String, String> paypalSdkConfig() {
-		Map<String, String> configMap = new HashMap<>();
-		configMap.put("mode", mode);
-		return configMap;
-	}
+    @Value("${paypal.client.secret}")
+    private String clientSecret;
 
-	@Bean
-	public APIContext apiContext() {
-		APIContext context = new APIContext(clientId, clientSecret, mode);
-		context.setConfigurationMap(paypalSdkConfig());
-		return context;
-	}
+    @Value("${paypal.mode}")
+    private String mode;
 
+    @Bean
+    public Map<String, String> paypalSdkConfig() {
+        Map<String, String> configMap = new HashMap<>();
+        configMap.put("mode", mode);
+        return configMap;
+    }
+
+    @Bean
+    public APIContext apiContext() {
+        APIContext context = new APIContext(clientId, clientSecret, mode);
+        context.setConfigurationMap(paypalSdkConfig());
+        return context;
+    }
 }
