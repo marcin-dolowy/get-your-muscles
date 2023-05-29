a722a8ca7ad3b99185955e1c31a1b79124384f62
Marcin Dołowy <marcindolowy00@gmail.com>
2023-05-29 16:17:24 +0200

    Fix event display in json

--- src/main/java/com/example/getyourmuscles/security/user/model/entity/User.java
+++ src/main/java/com/example/getyourmuscles/security/user/model/entity/User.java
@@ -1,10 +1,7 @@
 package com.example.getyourmuscles.security.user.model.entity;
 
 import com.example.getyourmuscles.event.model.Event;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.util.List;
@@ -16,7 +13,6 @@ import lombok.*;
 @NoArgsConstructor(force = true)
 @Entity
 @Table(name = "users")
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class User {
 
     @Id
@@ -43,10 +39,10 @@ public class User {
     private Role role;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JsonBackReference
+    @JsonIgnore
     private List<Event> memberEvents;
 
     @OneToMany(mappedBy = "trainer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JsonBackReference(value = "default")
+    @JsonIgnore
     private List<Event> trainerEvents;
 }
