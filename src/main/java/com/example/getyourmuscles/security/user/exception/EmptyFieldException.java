3a5534d52230607ec6e7e063ffaa5307074a55fd
Marcin Dołowy <marcindolowy00@gmail.com>
2023-06-02 01:13:44 +0200

    Fix error after register account with some empty fields

---
+++ src/main/java/com/example/getyourmuscles/security/user/exception/EmptyFieldException.java
@@ -0,0 +1,7 @@
+package com.example.getyourmuscles.security.user.exception;
+
+public class EmptyFieldException extends RuntimeException {
+    public EmptyFieldException(String message) {
+        super(message);
+    }
+}
