c3553b58a97d246442b8bf98dea602de48591ffc
Marcin Dołowy <marcindolowy00@gmail.com>
2023-06-01 15:25:31 +0200

    Add end point for finding last id event

--- src/main/java/com/example/getyourmuscles/event/repository/EventRepository.java
+++ src/main/java/com/example/getyourmuscles/event/repository/EventRepository.java
@@ -2,10 +2,13 @@ package com.example.getyourmuscles.event.repository;
 
 import com.example.getyourmuscles.event.model.Event;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findEventsByMemberId(Long id);
+
+    Optional<Event> findFirstByOrderByIdDesc();
 }
--- src/main/java/com/example/getyourmuscles/global/GlobalExceptionHandler.java
+++ src/main/java/com/example/getyourmuscles/global/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
-package com.example.getyourmuscles.security.user.exception;
+package com.example.getyourmuscles.global;
 
+import com.example.getyourmuscles.event.exception.EventNotFoundException;
+import com.example.getyourmuscles.security.user.exception.EmailAlreadyExistsException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,6 +11,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<String> handleEmailAlreadyExistsException(EmailAlreadyExistsException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(EventNotFoundException.class)
+    public ResponseEntity<String> handleEventNotFoundException(EventNotFoundException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
